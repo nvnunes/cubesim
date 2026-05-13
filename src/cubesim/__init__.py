b4c9c5e2306cc7b056f680046b4dcaa1f790bb13
Nelson Nunes <nvnunes@yorku.ca>
2026-05-13 13:55:03 -0400

    Add ETC API scaffold and PSF contract validation

--- src/cubesim/__init__.py
+++ src/cubesim/__init__.py
@@ -1,5 +1,6 @@
 """Public package entrypoints for cubesim."""
 
 from cubesim._version import __version__
+from cubesim.etc import EtcOptions, EtcResult, compute
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "EtcOptions", "EtcResult", "compute"]
